05e9366cf827ddef7212fc11b899bfacc5131db2
Pavlo Dzhyvak <pav8802@gmail.com>
2019-12-30 15:40:34 +0200

    stocks without modals

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,3 @@
-// $('.blog__slide').carousel({
-//     interval:false
-// });
-
-
 
 $('.blog__carousel').slick({
     dots: true,
@@ -12,16 +7,7 @@ $('.blog__carousel').slick({
     slidesToScroll: 3,
     arrows:false,
     responsive: [
-        // {
-        //     breakpoint: 1200,
-        //     settings: {
-        //         slidesToShow: 3,
-        //         slidesToScroll: 3,
-        //         infinite: true,
-        //         dots: true
-        //     }
-        // },
-        {
+             {
             breakpoint: 1087,
             settings: {
                 dots: true,
@@ -42,38 +28,6 @@ $('.blog__carousel').slick({
                 prevArrow: '<div class="arrow-wrapper arrow-wrapper-left"><i class="fas fa-chevron-left  carousel__arrow-left"></i></div>',
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
+
     ]
 });
-// ****************/
-// $('#recipeCarousel').carousel({
-//     interval: 10000
-// })
-//
-// $('.carousel .carousel-item').each(function(){
-//     var minPerSlide = 3;
-//     var next = $(this).next();
-//     if (!next.length) {
-//         next = $(this).siblings(':first');
-//     }
-//     next.children(':first-child').clone().appendTo($(this));
-//
-//     for (var i=0;i<minPerSlide;i++) {
-//         next=next.next();
-//         if (!next.length) {
-//             next = $(this).siblings(':first');
-//         }
-//
-//         next.children(':first-child').clone().appendTo($(this));
-//     }
-// });
-// // ****************
-//
-// (function($){
-//     "use strict";
-//     $('.next').click(function () { $('.carousel').carousel('next');return false;});
-//     $('.prev').click(function () { $('.carousel').carousel('prev');return false;});
-// })
-// (jQuery);

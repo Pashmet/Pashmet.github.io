5c56f083779a1971c67ea36a72bdb550d0c0d6a3
Pavlo Dzhyvak <pav8802@gmail.com>
2020-01-12 07:15:16 +0000

    Revert "Merge branch 'filtres3' into 'master'"
    This reverts merge request !27

--- dist/js/main.js
+++ dist/js/main.js
@@ -49,17 +49,3 @@ document.getElementById("basketInGallery").onclick = function () {
 
 // 0/1 basket
 
-
-$( function() {
-    $( "#slider-range" ).slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [ 75, 300 ],
-        slide: function( event, ui ) {
-            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-} );

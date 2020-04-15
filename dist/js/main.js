f30f9f31e34835224307e7997a5c9ea4c165e641
Pavlo Dzhyvak <pav8802@gmail.com>
2020-04-15 21:54:48 +0300

    modal to galery

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,24 @@
+// *furniture
 
 $('.furniture__qView').on('click', function () {
     let $srcImg = $(this).prev().attr('src');
 $('.details__img-img').attr('src', $srcImg);
 });
+
+
+// *furniture-galery
+
+$('.gallery-eye').on('click', function () {
+    let $srcImg = $(this)
+    .parent()
+    .children('.active')
+    .children().attr('src');
+$('.details__img-img').attr('src', $srcImg);
+console.log($('.details__img-img').attr('src'));
+console.log($(this).parent().children('.active').children().attr('src'));
+});
 // console.log($('.details__img-img').attr('src'));
+// console.log($srcImg)
 $(document).ready(loadFirstsItems);
 
 function loadFirstsItems() {
--- src/js/detailsModal.js
+++ src/js/detailsModal.js
@@ -1,6 +1,21 @@
+// *furniture
 
 $('.furniture__qView').on('click', function () {
     let $srcImg = $(this).prev().attr('src');
 $('.details__img-img').attr('src', $srcImg);
 });
+
+
+// *furniture-galery
+
+$('.gallery-eye').on('click', function () {
+    let $srcImg = $(this)
+    .parent()
+    .children('.active')
+    .children().attr('src');
+$('.details__img-img').attr('src', $srcImg);
+console.log($('.details__img-img').attr('src'));
+console.log($(this).parent().children('.active').children().attr('src'));
+});
 // console.log($('.details__img-img').attr('src'));
+// console.log($srcImg)
